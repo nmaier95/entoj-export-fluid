cab373ce7d1203339bd0499cea34aa9465855658
Christian Auth <c.auth@syzygy.de>
2018-01-11 13:55:54 +0100

    feat: adds simple config

--- source/index.js
+++ source/index.js
@@ -1,8 +1,29 @@
 /**
- *
+ * Registers with default configurations
+ */
+function register(configuration, options)
+{
+    // Commands
+    configuration.commands.add(require('./index.js').command.FluidExportCommand);
+
+    // Export
+    configuration.mappings.add(require('./index.js').export.FluidRenderer,
+        {
+            '!nodeRenderers': require('./index.js').export.renderer.rendererList
+        });
+    configuration.mappings.add(require('./index.js').export.FluidTransformer,
+        {
+            '!nodeTransformers': require('./index.js').export.transformer.transformerList
+        });
+}
+
+
+/**
+ * Public Api
  */
 module.exports =
 {
+    register: register,
     command: require('./command/index.js'),
     configuration: require('./configuration/index.js'),
     export: require('./export/index.js'),
