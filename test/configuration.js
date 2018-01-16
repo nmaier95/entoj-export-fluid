09f0ffccfdf67e08a554f0a137050009cd9b740e
Christian Auth <c.auth@syzygy.de>
2018-01-16 09:07:25 +0100

    test: removes chai pluins

--- test/configuration.js
+++ test/configuration.js
@@ -15,15 +15,3 @@ global.FLUID_TEST = __dirname;
 const chai = require('chai');
 chai.config.includeStack = true;
 global.expect = chai.expect;
-
-const chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
-
-const chaiSubset = require('chai-subset');
-chai.use(chaiSubset);
-
-const chaiString = require('chai-string');
-chai.use(chaiString);
-
-const chaiThings = require('chai-things');
-chai.use(chaiThings);
