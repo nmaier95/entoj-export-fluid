ff89790f1624469c2e63e1879e624e02eab9338b
Niklas Maier <maier-niklas@t-online.de>
2018-03-12 15:12:32 +0100

    feat(export.renderer.FluidFormOptionTagRenderer): add export support for custom option tag

---
+++ source/export/renderer/FluidFormOptionTagRenderer.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/**
+ * Requirements
+ * @ignore
+ */
+const FluidTagNodeRenderer = require('./FluidTagNodeRenderer.js').FluidTagNodeRenderer;
+const co = require('co');
+
+
+/**
+ * Render a custom tag node
+ *
+ * @memberOf export.renderer
+ * @extends Base
+ */
+class FluidFormOptionTagRenderer extends FluidTagNodeRenderer
+{
+    /**
+     * @inheritDoc
+     */
+    static get className()
+    {
+        return 'export.renderer/FluidFormOptionTagRenderer';
+    }
+
+
+    /**
+     * @return {String}
+     */
+    getArgumentName(index, argument, configuration)
+    {
+        return argument.name || 'param' + index;
+    }
+
+
+    /**
+     * @return {String}
+     */
+    getTagName(node, configuration)
+    {
+        return configuration.fluidConfiguration.builtinViewHelperNamespace + ':form.select.option';
+    }    
+
+
+    /**
+     * @inheritDoc
+     */
+    willRender(node, configuration)
+    {
+        return Promise.resolve(node && 
+            node.is('TagNode') && node.name === 'FormOptionTag');
+    }
+
+
+}
+
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports.FluidFormOptionTagRenderer = FluidFormOptionTagRenderer;
